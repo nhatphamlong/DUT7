b4635a1d6e67a7840174d02ce9e784297dcdb5f9
Pham Long Nhat <nhatphamlong.tt@gmail.com>
2021-12-01 17:54:38 +0700

    Trang chủ

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Anhlogo from './assets/Logo.png'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.upcontain}>
+        <Image source={Anhlogo} style={styles.Imagelogo}/>
+        <Text style={styles.texttit}>ĐIỀU ƯỚC THỨ 7</Text>
+        <Text style={styles.texttit}>CHUYÊN NBK</Text>
+      </View>
+      <View style={styles.downcontain}>
+        <TouchableOpacity onPress={() => alert('Đăng nhập nào!')} style={styles.button}>
+          <Text style={styles.textbutton}>ĐĂNG NHẬP</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => alert('Đăng ký nào!')} style={styles.button}>
+          <Text style={styles.textbutton}>ĐĂNG KÝ</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,8 +25,53 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  upcontain: {
+    flex:5,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'stretch',
+    //backgroundColor: '#ffffff',
+    height: '100%',
+    width: '100%',
+  },
+  Imagelogo: {
+    width: '35%',
+    height: '35%',
+    alignSelf: 'center',
+    marginBottom: 20,
+  },
+  texttit: {
+    color: '#FFD66D',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  downcontain: {
+    flex:5,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    //backgroundColor: '#ffffff',
+    height: '100%',
+    width: '100%',
+  },
+  button: {
+    backgroundColor: '#FDA743C9',
+    padding: 20,
+    borderRadius: 24,
+    alignSelf: 'center',
+    marginBottom: 10,
+    width: '70%',
+    height: '20%',
+  },
+  textbutton: {
+    color: '#ffffff',
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
 });
