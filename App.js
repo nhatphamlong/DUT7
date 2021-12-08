aad461c2e0dd0c3b95f2feeff2439b95e608e9e7
Pham Long Nhat <nhatphamlong.tt@gmail.com>
2021-12-08 18:39:49 +0700

    Teamwork

--- App.js
+++ App.js
@@ -1,100 +1,208 @@
 import React from 'react';
-import { ImageBackground, Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import Anhlogo from './components/image/Logo.png'; 
-import { LinearGradient } from 'expo-linear-gradient';
-import Anhnen from './components/image/anhnen.png';
+import {StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import Bannoidung from './components/image/noidung.png';
+import Bantruyenthong from './components/image/truyenthong.png';
+import Bankythuat from './components/image/kythuat.png';
+import Banhaucan from './components/image/haucan.png';
+import Homepage from './components/image/homepage.png';
+import Member from './components/image/member.png';
+import Charts from './components/image/charts.png';
+import Teamwork from './components/image/teamwork.png';
+import Messenger from './components/image/messenger.png';
 
-const Butt = (props) => {
-  return(
-    <View>
-      <LinearGradient // Button Linear Gradient
-          colors={['rgba(255, 178, 29, 0.8)','rgba(255, 132, 221, 0.4)']}
-          style={styles.button}
-        >
-          <TouchableOpacity onPress={() => alert(props.name)}>
-            <Text style={styles.text_button}>{props.name}</Text>
-          </TouchableOpacity>
-        </LinearGradient>
-    </View>
-  )
-};
-
-const Mainpage = () => {
-  return (
-    <View style={styles.container}>
-    <ImageBackground
-      source={Anhnen}
-      resizeMode="cover"
-      style={styles.backgound}
-    >    
-      <View style={styles.upcontain}>
-        <Image source={Anhlogo} style={styles.Imagelogo}/>
-        <Text style={styles.texttit}>ĐIỀU ƯỚC THỨ 7</Text>
-        <Text style={styles.texttit}>CHUYÊN HÙNG VƯƠNG</Text>
-      </View>
-      <View style={styles.downcontain}>
-        <Butt name="ĐĂNG NHẬP"/>
-        <Butt name="ĐĂNG KÝ"/>
-      </View>
-
-    </ImageBackground>
-    </View>
-  );
+const Image_logo = (name) => {
+  switch (name)
+  {
+    case "noidung": return(Bannoidung);
+    case "truyenthong" : return(Bantruyenthong);
+    case "kythuat" : return(Bankythuat);
+    case "haucan" : return(Banhaucan);
+  }
 }
 
-export default Mainpage;
+const Tit_team = (name) => {
+  switch (name)
+  {
+    case "noidung": return("BAN NỘI DUNG");
+    case "truyenthong" : return("BAN TRUYỀN THÔNG");
+    case "kythuat" : return("BAN KỸ THUẬT");
+    case "haucan" : return("BAN HẬU CẦN");
+  }
+}
+
+const Logo_team = (props) => {
+  return (
+    <View>
+      <Image source={Image_logo(props.name)} style={styles.image_logoteam} /> 
+      <Text style={styles.texttit}>{Tit_team(props.name)}</Text>
+    </View>
+  )
+}
+
+const Feature_left = () => {
+  return (
+    <View style={styles.left_container}>
+      <View style={[styles.squares, {backgroundColor: 'rgba(255, 216, 216, 0.5)'}]}>
+        <Text style={styles.text_squares}>TIMELINE</Text>
+      </View>
+      <View style={[styles.squares, {backgroundColor: 'rgba(183, 252, 141, 0.5)'}]}>
+        <Text style={styles.text_squares}>ĐÁNH GIÁ</Text>
+      </View>
+      <View style={[styles.squares, {backgroundColor: 'rgba(104, 224, 240, 0.5)'}]}>
+        <Text style={styles.text_squares}>HÌNH ẢNH</Text>
+      </View>
+    </View>
+  )
+}
+
+const Feature_right = () => {
+  return (
+    <View style={styles.left_container}>
+      <View style={[styles.squares, {backgroundColor: 'rgba(255, 196, 82, 0.5)'}]}>
+        <Text style={styles.text_squares}>PHÂN CHIA CÔNG VIỆC</Text>
+      </View>
+      <View style={[styles.squares, {backgroundColor: 'rgba(210, 84, 175, 0.5)'}]}>
+        <Text style={styles.text_squares}>SƠ ĐỒ THÀNH VIÊN</Text>
+      </View>
+      <View style={[styles.squares, {backgroundColor: 'rgba(247, 255, 149, 0.5)'}]}>
+        <Text style={styles.text_squares}>BIÊN BẢN HỌP NỘI BỘ</Text>
+      </View>
+    </View>
+  )
+}
+
+const Image_navibut = (name) => {
+  switch (name)
+  {
+    case "Homepage": return(Homepage);
+    case "Member": return(Member);
+    case "Charts": return(Charts);
+    case "Teamwork": return(Teamwork);
+    case "Messenger": return(Messenger);
+  }
+}
+
+const Navigation_but = (props) => {
+  return(
+    <View style={styles.button}>
+      <TouchableOpacity onPress={() => alert(props.name)}>
+        <Image 
+          source={Image_navibut(props.name)} 
+          style={styles.image_button}
+        />
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const Navigation_bar = () => {
+  return (  
+    <View style={styles.navigation_bar}>
+      <Navigation_but name="Homepage" />
+      <Navigation_but name="Member" />
+      <Navigation_but name="Charts" />
+      <Navigation_but name="Teamwork" />
+      <Navigation_but name="Messenger" />
+    </View>
+  )
+}
+
+const ABC_Team = (props) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.up_container}>
+        <Logo_team name="truyenthong" />
+      </View>
+      <View style={styles.down_container}>
+        <Feature_left />
+        <Feature_right />
+      </View>
+      <View style={styles.navigation_bar}>
+        <Navigation_bar />
+      </View>
+    </View>
+  )
+}
+
+export default ABC_Team;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  backgound: {
-    flex: 1,
+    backgroundColor: '#EEF1FC',
     justifyContent: 'center',
-  },
-  upcontain: {
-    flex:5,
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
     alignItems: 'stretch',
-    //backgroundColor: '#ffffff',
-    height: '100%',
-    width: '100%',
   },
-  Imagelogo: {
-    width: '35%',
-    height: '35%',
+  up_container: {
+    flex: 3,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'flex-end',
+  },
+  image_logoteam: {
+    width: '50%',
+    height: '50%',
     alignSelf: 'center',
-    marginBottom: 20,
   },
   texttit: {
-    color: '#FFD66D',
+    color: '#000000',
     fontSize: 30,
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  downcontain: {
-    flex:5,
+  down_container:{
+    flex: 7, 
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+  },
+  left_container: {
+    flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'stretch',
-    //backgroundColor: '#ffffff',
-    height: '100%',
-    width: '100%',
+    alignItems: 'center',
+  },
+  right_container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  squares: {
+    width: 150,
+    height: 140,
+    alignSelf: 'center',
+    marginBottom: 20,
+    justifyContent: 'center',
+    borderRadius: 15,
+  },
+  text_squares: {
+    width: 120,
+    color: '#000000',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    alignSelf: 'center',
+  },
+  navigation_bar: {
+    flex: 1, 
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   button: {
-    width: 200,
-    height: 70,
-    marginTop: 20,
-    marginBottom: 10,
-    padding: 20,
-    borderRadius: 24,
-    alignSelf:'center',
+    width: 45, 
+    height: 45, 
+    //borderColor: 'rgba(236, 116, 170,0.8)',
+    borderRadius: 5,
+    //borderWidth: 1,
+    marginLeft: 10, 
+    marginRight: 10,
   },
-  text_button: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: '#ffffff',
-    textAlign: 'center',
-  },
+  image_button:{
+    width: 35,
+    height: 35,
+    justifyContent: 'flex-end',
+    alignSelf: 'center',
+  }
 });
